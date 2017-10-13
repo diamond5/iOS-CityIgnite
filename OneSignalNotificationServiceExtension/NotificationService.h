0bf64b8a9568c4a4b66f11391b33616fa8f50e5f
diamond5 <masterofworld20000@gmail.com>
2017-10-12 22:31:48 -0400

    implement the push notification using Onesignal

---
+++ OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by eden on 4/4/17.
+//  Copyright © 2017 dmc. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
