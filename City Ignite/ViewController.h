8fd01d19278f8dac21036b4c26c851ac016bc0a5
diamond5 <masterofworld20000@gmail.com>
2017-10-12 22:20:10 -0400

    Add ViewController

---
+++ City Ignite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  City Ignite
+//
+//  Created by eden on 4/3/17.
+//  Copyright © 2017 dmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ City Ignite/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  City Ignite
+//
+//  Created by eden on 4/3/17.
+//  Copyright © 2017 dmc. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AppDelegate.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (AppDelegate*)appDelegate {
+    return (AppDelegate*)[[UIApplication sharedApplication] delegate];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+
+@end
